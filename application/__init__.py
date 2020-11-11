cde0bffc3d7e8635482d02931dd486d1e84671e1
Alex Smith <smithalexanderbradley@gmail.com>
2020-11-11 15:57:50 +0000

    messing around

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 app = Flask(__name__)
 
-app.config['DBI_URI'] = getenv('DBI_URI')
+app.config['DBI_URI'] = "mysql+pymysql://root:password1234@34.105.157.94/walls"
 app.config['SECRET_KEY'] = 'a-secret-key'
 db = SQLAlchemy(app)
 
--- application/routes.py
+++ application/routes.py
@@ -5,21 +5,7 @@ from application.forms import WallForm, OrderWall, LocationForm, ActivityForm
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    form = OrderWall()
-    totals = {
-        "total_walls": Wall.query.count(),
-        "total_locations": Location.query.count(),
-        "total_activites": Activity.query.count()
-    }
-    if form.order_with.data == "id":
-        wall = Wall.query.order_by(Wall.id.desc()).all()
-    elif form.order_with.data == "location":
-        wall = Wall.query.order_by(Wall.location_id.desc()).all()
-    elif form.order_with.data == "activity":
-        wall = Wall.query.order_by(Wall.activity_id).all()
-    else:
-        wall = Wall.query.all()
-    return render_template('index.html', title="WallFinder App", wall=wall, form=form, totals=totals)
+    return render_template('index.html', title="WHERE IS THE WALL")
 
 @app.route('/add', methods=['POST', 'GET'])
 def add():
@@ -31,16 +17,4 @@ def add():
         db.session.add(todo)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('add.html', title="New Location", form=form)
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    form = WallForm()
-    wall = Wall.query.get(id)
-    if form.validate_on_submit():
-        wall.walls = form.walls.data
-        db.session.commit()
-        redirect(url_for('index'))
-    elif request.method == 'GET':
-        form.walls.data = wall.walls
-    return render_template('update.html', title='Edit your wall', form=form)    
+    return render_template('add.html', title="New Location", form=form)   
