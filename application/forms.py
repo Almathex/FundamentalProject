85d1456630586a85cbb207475ad7d6be3b3a9abc
Alex-Smith <smithalexanderbradley@gmail.com>
2020-11-11 14:35:16 +0000

    Changed routes and added forms

--- application/forms.py
+++ application/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, ValidationError
 
-from application.models import Wall
+from application.models import Wall, Location, Activity
 
 
 class WallForm(FlaskForm):
@@ -17,6 +17,35 @@ class WallForm(FlaskForm):
         walls = Wall.query.all()
         for index in walls:
             if index.wall == wall.data:
+                raise ValidationError('This wall already exists!')
+
+class LocationForm(FlaskForm):
+    location = StringField('Location:',
+        validators = [
+            DataRequired(),
+        ]    
+    )
+    submit = SubmitField('Submit')
+
+    def validate_task(self, location):
+        locations = Location.query.all()
+        for index in locations:
+            if index.location == location.data:
+                raise ValidationError('This wall already exists!')            
+
+
+class ActivityForm(FlaskForm):
+    activity = StringField('Activity:',
+        validators = [
+            DataRequired(),
+        ]    
+    )
+    submit = SubmitField('Submit')
+
+    def validate_task(self, activity):
+        activities = Activity.query.all()
+        for index in activities:
+            if index.activity == activity.data:
                 raise ValidationError('This wall already exists!')
 
 class OrderWall(FlaskForm):
