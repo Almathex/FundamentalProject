1398abfc9a09d72a4387b9bad388a9d44ee476e7
Alex-Smith <smithalexanderbradley@gmail.com>
2020-11-11 16:08:26 +0000

    deleted some add route bits

--- application/routes.py
+++ application/routes.py
@@ -9,12 +9,4 @@ def index():
 
 @app.route('/add', methods=['POST', 'GET'])
 def add():
-    form = LocationForm()
-    if form.validate_on_submit():
-        location = Location(
-            locations = form.location.data,
-        )
-        db.session.add(todo)
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('add.html', title="New Location", form=form)   
+    return render_template('add.html', title="New Location")   
