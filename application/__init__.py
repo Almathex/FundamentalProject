8629dad93d6c8435063156add87d6d4ec4cc0963
Alex Smith <smithalexanderbradley@gmail.com>
2020-11-12 10:11:22 +0000

    added requirements.txt

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 app = Flask(__name__)
 
-app.config['DBI_URI'] = "mysql+pymysql://root:password1234@34.105.157.94/walls"
-app.config['SECRET_KEY'] = 'a-secret-key'
+app.config['DBI_URI'] = getenv('DBI_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 from application import routes
