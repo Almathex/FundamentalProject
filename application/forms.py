2f7e045c95c55212f90dd937f540ae726e5cf169
Alex-Smith <smithalexanderbradley@gmail.com>
2020-11-12 10:58:38 +0000

    changed forms

--- application/forms.py
+++ application/forms.py
@@ -1,52 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, SelectField
+from wtforms import StringField, IntegerField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, ValidationError
 
 from application.models import Wall, Location, Activity
 
 
 class WallForm(FlaskForm):
-    wall = StringField('Wall:',
-        validators = [
-            DataRequired(),
-        ]    
-    )
+    wall = StringField('Wall name:', validators = [ DataRequired()])
+    location = StringField('Location:', validators = [ DataRequired()])
+    postcode = StringField('Postcode:', validators = [ DataRequired()])
+    size = StringField('Size:', validators = [ DataRequired()])
+    rating = IntegerField('Rating:', validators = [ DataRequired()])
     submit = SubmitField('Submit')
-
-    def validate_wall(self, wall):
-        walls = Wall.query.all()
-        for index in walls:
-            if index.wall == wall.data:
-                raise ValidationError('This wall already exists!')
-
-class LocationForm(FlaskForm):
-    location = StringField('Location:',
-        validators = [
-            DataRequired(),
-        ]    
-    )
-    submit = SubmitField('Submit')
-
-    def validate_location(self, location):
-        locations = Location.query.all()
-        for index in locations:
-            if index.location == location.data:
-                raise ValidationError('This wall already exists!')            
-
 
 class ActivityForm(FlaskForm):
-    activity = StringField('Activity:',
-        validators = [
-            DataRequired(),
-        ]    
-    )
+    activity = StringField('Activity:', validators = [DataRequired(),] )
+    additional_equiptment = BooleanField('Need Equiptment:', validators = [ DataRequired()] )
     submit = SubmitField('Submit')
 
-    def validate_activity(self, activity):
-        activities = Activity.query.all()
-        for index in activities:
-            if index.activity == activity.data:
-                raise ValidationError('This wall already exists!')
+def validate_activity(self, activity):
+    activities = Activity.query.all()
+    for index in activities:
+        if index.activity == activity.data:
+            raise ValidationError('This activity already exists!')
 
 class OrderWall(FlaskForm):
     order_with = SelectField('Order With:',
