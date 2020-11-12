08da0d09a05c972d6cf81ad245a1575a89935d4e
Alex-Smith <smithalexanderbradley@gmail.com>
2020-11-12 11:20:13 +0000

    removed location form

--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,23 @@
 from flask import render_template, url_for, redirect, request
 from application import app, db
 from application.models import Wall, Location, Activity
-from application.forms import WallForm, OrderWall, LocationForm, ActivityForm
+from application.forms import WallForm, OrderWall, ActivityForm
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+
     return render_template('index.html', title="WHERE IS THE WALL")
 
 @app.route('/add', methods=['POST', 'GET'])
 def add():
-    return render_template('add.html', title="New Location")   
+    form = WallForm()
+    if form.validate_on_submit():
+        walls = Walls(
+            wall = form.wall.data,
+            complete = False
+        )
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for('index'))
+     form = WallForm()
+    return render_template('add.html', title="New Location", form=form)   
