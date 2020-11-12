1f2256747761a3d008c5b361445e5783eae979f2
Alex-Smith <smithalexanderbradley@gmail.com>
2020-11-12 13:10:06 +0000

    layout change

--- application/models.py
+++ application/models.py
@@ -1,12 +1,6 @@
 from application import db
 
-class Walls(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
-    activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'), nullable =False)
-    nickname = db.Column(db.String(30), unique=True)
-    wall_size = db.Column(db.String(30), unique=True)
-    rating = db.Column(db.Float(5), nullable=True)
+
 
 class Locations(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,3 +14,11 @@ class Activities(db.Model):
     activity_name =  db.Column(db.String(50), unique=True)
     Additional_equiptment = db.Column(db.Boolean, nullable=False)
     activities = db.relationship('Wall', backref = 'activity')
+    
+class Walls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+    activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'), nullable =False)
+    nickname = db.Column(db.String(30), unique=True)
+    wall_size = db.Column(db.String(30), unique=True)
+    rating = db.Column(db.Float(5), nullable=True)
