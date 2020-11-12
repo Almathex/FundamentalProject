c99013ed98a3875ede2698437fc90ac09d260fce
Alex-Smith <smithalexanderbradley@gmail.com>
2020-11-12 14:59:36 +0000

    take 10

--- application/forms.py
+++ application/forms.py
@@ -18,7 +18,7 @@ class LocationForm(FlaskForm):
 
 class ActivityForm(FlaskForm):
     activity_name =  StringField('Activity:', validators = [DataRequired()])
-    additional_equiptment = IntegerField('Equiptment required?:', validators = [DataRequired()])
+    additional_equiptment = BooleanField('Equiptment required?:', validators = [DataRequired()])
     submit = SubmitField('Submit')
 
     def validate_task(self, task):
