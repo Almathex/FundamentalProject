3831982150776da917011da8f71545cdf79bc7b2
Alex-Smith <smithalexanderbradley@gmail.com>
2020-11-12 13:14:27 +0000

    models

--- application/models.py
+++ application/models.py
@@ -7,18 +7,18 @@ class Locations(db.Model):
     county = db.Column(db.String(50), nullable=False)
     town = db.Column(db.String(50), nullable=False)
     postcode = db.Column(db.String(10), nullable=False)
-    loctaions = db.relationship('Wall', backref = 'Locations')
+    loctaions = db.relationship('Wall', backref = 'locations')
     
 class Activities(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     activity_name =  db.Column(db.String(50), unique=True)
     Additional_equiptment = db.Column(db.Boolean, nullable=False)
-    activities = db.relationship('Wall', backref = 'Activities')
+    activities = db.relationship('Wall', backref = 'activities')
     
 class Walls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    location_id = db.Column(db.Integer, db.ForeignKey('Locations.id'), nullable=False)
-    activity_id = db.Column(db.Integer, db.ForeignKey('Acivities.id'), nullable =False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'), nullable =False)
     nickname = db.Column(db.String(30), unique=True)
     wall_size = db.Column(db.String(30), unique=True)
     rating = db.Column(db.Float(5), nullable=True)
